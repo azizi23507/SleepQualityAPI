3647bfea320b69f0900029b30c05aecb15c0d039
azizi23507 <abdul.azizi@stud.th-deg.de>
2026-08-22 20:07:19 +0200

    add docker setup, requirements, config for containerization

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Settings(BaseSettings):
     database_url: str
     api_key: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 settings = Settings()
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import settings
-engine = create_engine(settings.database_url).connect()
+engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base class all ORM models inherit from — registers them with SQLAlchemy
